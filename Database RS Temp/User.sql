5d0e80f603718468906610e83807350be8127377
Isai10 <Isai10.th@hotmail.com>
2021-07-28 16:40:56 -0500

    Creación de tabla User y Rol en la base de datos

---
+++ Database RS Temp/User.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[id] INT NOT NULL PRIMARY KEY,
+	[name] VARCHAR(60) NOT NULL,
+	[last_name] VARCHAR(60) NOT NULL,
+	[email] VARCHAR(320) NOT NULL,
+	[phone] INT NOT NULL, 
+    [rol_id] INT NOT NULL, 
+    CONSTRAINT [FK_User_Rol] FOREIGN KEY ([rol_id]) REFERENCES [User]([id])
+)
